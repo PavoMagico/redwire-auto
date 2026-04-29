0036f4c30d652e81831ad52333db36df2afc7ed3
Santiago de Pablo de Castro <santidpdc@gmail.com>
2026-04-29 11:15:43 +0100

    feat: support FRONTEND_URL env var in CORS config

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const routes  = require('./routes/index');
 const app  = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use('/api', routes);
 app.get('/', (_, res) => res.json({ status: 'Red Wire Auto API v1.0 running' }));
